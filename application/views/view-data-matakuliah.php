fc8ce8b31453ef474992f4ea39a3f7a8c21cfba0
WP2-Malvhark <semangatt111@gmail.com>
2021-10-29 10:38:17 +0700

    Menambahkan form validation & Pustaka-Booking

--- application/views/view-data-matakuliah.php
+++ application/views/view-data-matakuliah.php
@@ -1,48 +1,39 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tampil Data Matakuliah</title>
 </head>
 <body>
+<p><?php echo anchor('form', 'Try it again!'); ?></p>
+
     <center>
         <table>
             <tr>
-                <th colspan="3">
-                    Tampil Data Mata Kuliah
-                </th>
-            </tr>
-            <tr>
-                <td colspan="3">
-                    <hr>
-                </td>
+                <th colspan="3">Tampil Data Mata Kuliah</th>
             </tr>
             <tr>
                 <th>Kode MTK</th>
                 <th>:</th>
-                <td>
-                    <?= $kode; ?>
-                </td>
+                <td><?=$kode; ?></td>
             </tr>
             <tr>
-                <td>Nama MTK</td>
+                <th>Nama MTK</th>
                 <td>:</td>
-                <td>
-                    <?= $nama; ?>
-                </td>
+                <td><?=$nama; ?></td>
             </tr>
             <tr>
-                <td>SKS</td>
+                <th>sks</th>
                 <td>:</td>
-                <td>
-                    <?= $sks; ?>
-                </td>
-            </tr>
+                <td><?=$sks; ?></td>
+            </tr> 
             <tr>
-                <td colspan="3" align="center">
-                    <a href="<?= base_url('matakuliah'); 
-                    ?>">Kembali</a>
-                </td>
-            </tr>
-         </table>
+                <td colspan="3" align="center"><a href="<?=base_url('matakuliahs');?>">Kembali </a></td>
+            </tr>           
+        </table>
     </center>
+    
 </body>
 </html>
