cdf8108b409878c0f8c1b074ad1958eeadc4c668
WP2-Malvhark <semangatt111@gmail.com>
2022-04-07 09:02:51 +0700

    WP3 Pertemuan 4

--- application/controllers/Booking.php
+++ application/controllers/Booking.php
@@ -28,11 +28,10 @@ class Booking extends CI_Controller
         $dtb = $this->ModelBooking->showtemp(['id_user' => $id_user])->num_rows();
         
         if ($dtb < 1) {
-            $this->session->set_flashdata('pesan', '<div class="alert alert-massege alert-danger" role="alert">Tidak Ada Buku dikeranjang</div>');
+            $this->session->set_flashdata('pesan', '<div class="alert alert-massege alert-danger" role="alert">Tidak Ada Buku dikeranjang</div>'); 
             redirect(base_url());
         } else {
-            $data['temp'] = $this->db->query("select image, judul_buku, penulis, penerbit, tahun_terbit,id_buku from te
-            mp where id_user='$id_user'")->result_array();
+            $data['temp'] = $this->db->query("select image, judul_buku, penulis, penerbit, tahun_terbit,id_buku from temp where id_user='$id_user'")->result_array();
         }
             $data['judul'] = "Data Booking";
             
@@ -40,9 +39,9 @@ class Booking extends CI_Controller
             $this->load->view('booking/data-booking', $data);
             $this->load->view('templates/templates-user/modal');
             $this->load->view('templates/templates-user/footer');
-    }
-
-public function tambahBooking()
+        
+        }
+    public function tambahBooking()
     {
         $id_buku = $this->uri->segment(3);
         //memilih data buku yang untuk dimasukkan ke tabel temp/keranjang melaluivariabel $isi
@@ -92,12 +91,83 @@ public function tambahBooking()
         }
         
         //membuat tabel temp jika belum ada
-        $this->ModelBooking->createTemp();
         $this->ModelBooking->insertData('temp', $isi);
         
         //pesan ketika berhasil memasukkan buku ke keranjang
         $this->session->set_flashdata('pesan', '<div class="alert alert-success alert-message" role="alert">Buku berhasil ditambahkan ke keranjang </div>');
         redirect(base_url() . 'home');
     }
-       
+
+    public function hapusbooking()
+    {
+        $id_buku = $this->uri->segment(3);
+        $id_user = $this->session->userdata('id_user');
+        
+        $this->ModelBooking->deleteData(['id_buku' => $id_buku], 'temp');
+        $kosong = $this->db->query("select*from temp where id_user='$id_user'")->num_rows();
+        if ($kosong < 1) {
+            $this->session->set_flashdata('pesan', '<div class="alert alert=massege alert-danger" role="alert">Tidak Ada Buku dikeranjang</div>');
+            redirect(base_url());
+        } else {
+            redirect(base_url() . 'booking');
+        }
+    }
+
+    public function bookingSelesai($where)
+    {
+        //mengupdate stok dan dibooking di tabel buku saat proses booking diselesaikan
+        $this->db->query("UPDATE buku, temp SET buku.dibooking=buku.dibooking+1, buku.stok=buku.stok-1 WHERE buku.id=temp.id_buku");
+            
+            $tglsekarang = date('Y-m-d');
+            $isibooking = [
+                'id_booking' => $this->ModelBooking->kodeOtomatis('booking', 'id_booking'),
+                'tgl_booking' => date('Y-m-d H:m:s'),
+                'batas_ambil' => date('Y-m-d', strtotime('+2 days', strtotime($tglsekarang))),
+                'id_user' => $where
+            ];
+             //menyimpan ke tabel booking dan detail booking, dan mengosongkan tabel t emporari
+            $this->ModelBooking->insertData('booking', $isibooking);
+            $this->ModelBooking->simpanDetail($where);
+            $this->ModelBooking->kosongkanData('temp');
+            
+            redirect(base_url() . 'booking/info');
+    }
+
+    public function info()
+    {
+        $where = $this->session->userdata('id_user');
+        $data['user'] = $this->session->userdata('nama');
+        $data['judul'] = "Selesai Booking";
+        $data['useraktif'] = $this->ModelUser->cekData(['id' => $this->session->userdata('id_user')])->result();
+        $data['items'] = $this->db->query("select*from booking bo, booking_detail d, buku bu where d.id_booking=bo.id_booking and d.id_buku=bu.id and bo.id_user='$where'")->result_array();
+        $this->load->view('templates/templates-user/header', $data);
+        $this->load->view('booking/info-booking', $data);
+        $this->load->view('templates/templates-user/modal');
+        $this->load->view('templates/templates-user/footer');
+    }
+
+
+    public function exportToPdf()
+    {
+        $id_user = $this->session->userdata('id_user');
+        $data['user'] = $this->session->userdata('nama');
+        $data['judul'] = "Cetak Bukti Booking";
+        $data['useraktif'] = $this->ModelUser->cekData(['id' => $this->session->userdata('id_user')])->result();
+        $data['items'] = $this->db->query("select*from booking bo, booking_detail d, buku bu where d.id_booking=bo.id_booking and d.id_buku=bu.id and bo.id_user='$id_user'")->result_array();
+        
+        $this->load->library('dompdf_gen');
+        $this->load->view('booking/bukti-pdf', $data);
+ 
+        $paper_size = 'A4'; // ukuran kertas
+        $orientation = 'landscape'; //tipe format kertas potrait atau landscape
+        $html = $this->output->get_output();
+        
+        $this->dompdf->set_paper($paper_size, $orientation);
+        //Convert to PDF
+        $this->dompdf->load_html($html);
+        $this->dompdf->render();
+        $this->dompdf->stream("bukti-booking-
+        $id_user.pdf", array('Attachment' => 0));
+        // nama file pdf yang di hasilkan
+    }
 }
